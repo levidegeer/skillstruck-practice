bbc31e255ad7cdf0719a673daf73979eaf45ebb8
levidegeer <levidegeer@gmail.com>
2024-08-16 13:58:34 -0700

    exported from Skill Struck

---
+++ object oriented programming.py
@@ -0,0 +1,40 @@
+# class Person:
+#     def __init__(self, name, age, gender):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+
+# class Dog:   
+#    def __init__(self, name, age, gender):
+#          self.name = name
+#          self.age = age
+#          self.gender = gender
+
+# class Car:
+#     def __init__(self, kind, color, year):
+#         self.kind = kind
+#         self.color = color
+#         self.year = year
+
+# class Tree:
+#     def __init__(self, fruit, height, leaf, flower, seed):
+#         self.fruit = fruit
+#         self.height = height
+#         self.leaf = leaf
+#         self.flower = flower
+#         self.seed = seed
+
+class Dessert:
+    def __init__(self, shape, size, filling, topping):
+        self.shape = shape
+        self.size = size
+        self.filling = filling
+        self.topping = topping
+
+
+
+
+
+
+
+
