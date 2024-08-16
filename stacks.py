5e67e9636e095fc661bb9614a65dfcc14fd310bd
levidegeer <levidegeer@gmail.com>
2024-08-16 13:58:38 -0700

    exported from Skill Struck

---
+++ stacks.py
@@ -0,0 +1,37 @@
+# stack = []
+# stack.append("a")
+# stack.append("b")
+# stack.append("c")
+# stack.append("d")
+# stack.pop()
+# print(stack)
+
+# stack = []
+# first = "r"
+# second = "t"
+# third = "s"
+# fourth = "y"
+# fifth = "o"
+# stack.append(third)
+# stack.append(second)
+# stack.append(fifth)
+# stack.append(first)
+# stack.append(fourth)
+# print(stack)
+
+# answer = ["apples", "steak", "potatoes", "carrots"]
+# x = input()
+# if "s" in x:
+#     answer.append(x)
+#     print(answer)
+# else:
+#     print("The input doesn't have the letter s")
+
+answer = ["c", "r", "o", "a", "k"]
+
+answer.pop()
+answer.pop()
+answer.pop
+answer.append("a")
+answer.append("b")
+print(answer)
