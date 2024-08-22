725fb593992df5b63fdc2a77dfccf849a60668ea
levidegeer <levidegeer@gmail.com>
2024-08-22 11:50:17 -0700

    exported from Skill Struck

---
+++ Further Python Learning/Printing Specifics in Python.py
@@ -0,0 +1,15 @@
+# print("test \n test")
+# print("test \t test")
+# print("test \r test")
+
+# print("Freshman")
+# print("\t Sophomore")
+# print("\t \t Junior")
+# print("\t \t \t Senior")
+
+# print("Once \n Upon \n a \n Time")
+
+print("Test \n Test \t Test")
+
+
+
