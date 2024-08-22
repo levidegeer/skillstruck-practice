51993a58ee8f3996d93aa9537151e90c7b7d56e8
levidegeer <levidegeer@gmail.com>
2024-08-22 11:50:15 -0700

    exported from Skill Struck

---
+++ Further Python Learning/Python Math Library.py
@@ -0,0 +1,24 @@
+import math
+# print(math.ceil(3.2))
+# print(math.floor(3.2))
+# print(math.sqrt(16))
+# print(math.pow(4, 3))
+
+# p = int(input("Number of people: "))
+# b = int(input("Number of bikes: "))
+# r = p / b
+# x = math.ceil(r)
+# print("In this town, for every bike that exists there are " + str(x) + " people.")
+
+# l = int(input("Length of the cube: "))
+# print(int(math.pow(l, 3)))
+
+# x = int(input("value of stock at the beginning: "))
+# y = int(input("value of stock at the end: "))
+# rate = math.sqrt(y / x) - 1
+# print(round(rate, 2))
+
+a = int(input("Length of side a: "))
+b = int(input("Length of side b: "))
+c = math.sqrt(a**2 + b**2)
+print(round(c, 2))
