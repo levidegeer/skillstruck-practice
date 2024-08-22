06fee6ee9fcf092667e3e92eb99527195d788ced
levidegeer <levidegeer@gmail.com>
2024-08-22 11:50:18 -0700

    exported from Skill Struck

---
+++ Further Python Learning/Different Concatenating Approaches.py
@@ -0,0 +1,34 @@
+# name = "Levi"
+# dogs = 1
+# location = "Arizona"
+# print("My name is " + name + " and I have " + str(dogs) + " dogs and live in " + location)
+# print(f"My name is {name} and I have {dogs} dogs and live in {location}")
+# print("My name is", name, "and I have", dogs, "dogs and live in", location)
+
+# string1 = "a"
+# string2 = "b"
+# string3 = "c"
+# string4 = "d"
+# string5 = "e"
+# int1 = 1
+# int2 = 2
+# int3 = 3
+# int4 = 4
+# int5 = 5
+# print(f"{string1}{string2}{string3}{string4}{string5}{int1}{int2}{int3}{int4}{int5}")
+
+# football = 21
+# soccer = 2
+# basketball = 85
+# baseball = 5
+# tennis = 14
+# print(football, soccer, basketball, baseball, tennis)
+
+c = input("What is your preferred method of concatenation ")
+if c == "+":
+    print("I prefer the " + c + " method")
+elif c == "f":
+    print(f"I prefer the {c} method")
+elif c == "comma" or c == ",":
+    print("I prefer the", c,"method")
+
