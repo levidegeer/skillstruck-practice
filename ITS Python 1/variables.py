18febb28deca88dabbca5b6cc48da98872bab439
levidegeer <levidegeer@gmail.com>
2024-08-23 12:16:20 -0700

    exported from Skill Struck

--- ITS Python 1/variables.py
+++ ITS Python 1/variables.py
@@ -1,8 +1,22 @@
-var1 = "1"
-var2 = "2"
-var3 = "3"
-print(var1)
-print(var2)
-print(var3)
+# var1 = "1"
+# var2 = "2"
+# var3 = "3"
+# print(var1)
+# print(var2)
+# print(var3)
 
+# how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood = 5
+# print(how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood)
+
+def reverse_words(s):
+    lst = s.split()
+    lst2 = []
+    string = ""
+    for x in lst:
+        lst2.insert(0, x)
+    for x in lst2:
+        string = string + " " + x
+    print(string)
+
+reverse_words("Hello world!")
 
