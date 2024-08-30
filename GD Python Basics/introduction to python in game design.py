6afb51cd96bddc11491fb91001dbde0c3bcfb34f
levidegeer <levidegeer@gmail.com>
2024-08-30 12:06:36 -0700

    exported from Skill Struck

---
+++ GD Python Basics/introduction to python in game design.py
@@ -0,0 +1,19 @@
+# print("Hello World")
+
+# print("I love to code!")
+
+# print("Orange")
+# print("Red")
+# print("Yellow")
+
+# print("pancakes")
+# print("fries")
+# print("lemonade")
+
+# print("Coding is very detail oriented. Every symbol counts in a programming language, so the autograder is also very detail oriented. Here are some tips and tricks to make sure your correct code will pass the checkpoints and challenges. Extra sentence one. Extra sentence two.")
+
+# print("a")
+# print("b")
+# print("c")
+# print("d")
+# print("e")
