311012a6751986d40d0142b8ac31a8197096adff
levidegeer <levidegeer@gmail.com>
2024-08-30 12:06:43 -0700

    exported from Skill Struck

---
+++ ITS Python 1/syntax and comments.py
@@ -0,0 +1,30 @@
+# this is a string
+# a = "a"
+
+# because
+# sound = "a"
+# because
+# color = "b"
+# because
+# flavor = "c"
+
+# morning = input()
+# afternoon = input()
+# evening = input()
+# print(morning)
+# print(afternoon)
+# print(evening)
+
+# print("a")
+# a
+# print("b")
+# b
+# print("c")
+# b
+
+# print("a")
+# a
+# print("b")
+# b
+# print("c")
+# b
