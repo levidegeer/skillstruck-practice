7a0ce8e06ed118fec8514578b1f7deafa1a7d26a
levidegeer <levidegeer@gmail.com>
2024-08-30 12:06:35 -0700

    exported from Skill Struck

--- ITS Python 1/variables.py
+++ ITS Python 1/variables.py
@@ -8,15 +8,24 @@
 # how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood = 5
 # print(how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood)
 
-def reverse_words(s):
-    lst = s.split()
-    lst2 = []
-    string = ""
-    for x in lst:
-        lst2.insert(0, x)
-    for x in lst2:
-        string = string + " " + x
-    print(string)
+# a = 50
+# b = 10
+# c = 1000
+# print(a)
+# print(b)
+# print(c)
 
-reverse_words("Hello world!")
+# hero1 = "a"
+# hero2 = "b"
+# hero3 = "c"
+# print(hero1)
+# print(hero2)
+# print(hero3)
 
+what_goes_up_when_rain_comes_down = "humidity"
+what_is_full_of_holes_but_holds_water = "sponge"
+what_can_you_not_keep_until_you_give_it = "promise"
+
+print(what_goes_up_when_rain_comes_down)
+print(what_is_full_of_holes_but_holds_water)
+print(what_can_you_not_keep_until_you_give_it)
