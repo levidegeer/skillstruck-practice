24a1e5c303edbbc260f89203f81c7cb8e7e8a8ce
levidegeer <levidegeer@gmail.com>
2024-08-30 12:06:44 -0700

    exported from Skill Struck

---
+++ ITS Python 1/variable types.py
@@ -0,0 +1,29 @@
+# string = "a"
+# integer = 1
+# boolean = True
+# print(string)
+# print(integer)
+# print(boolean)
+
+# var = True
+# var1 = False
+
+# var = True
+# var1 = "Hello!"
+# var2 = 42
+# print(var)
+# print(var1)
+# print(var2)
+
+# age = input("How old are you? ")
+# print("You are " + age + " years old.")
+
+# did_do = "a"
+# wish_you_did = "b"
+# meals = 3
+# homework = 5
+# first = True
+# second = False
+
+
+
