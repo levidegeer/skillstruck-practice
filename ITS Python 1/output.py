dbe3481659b53e6a27b42684ec21939f18dfe7da
levidegeer <levidegeer@gmail.com>
2024-08-30 12:06:43 -0700

    exported from Skill Struck

---
+++ ITS Python 1/output.py
@@ -0,0 +1,25 @@
+# color = input()
+# print("Your favorite color is " + color)
+
+# name = input()
+# print("Hello, " + name + "!")
+
+# flavor = input()
+# mix_in = input()
+# topping = input()
+# print(flavor + mix_in + topping)
+
+# name = input()
+# comp = input()
+# print("Hello " + name + " I would like to give you a compliment. " + comp)
+
+# name = input()
+# exp = input()
+# print("Hello " + name + " welcome to the guitar class! Your music experience: " + exp)
+
+# a = input()
+# b = input()
+# c = input()
+# d = input()
+# e = input()
+# print(c)
