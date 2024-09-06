8e1b8efc902a2ba13b9c126d55cd59515d025d69
levidegeer <levidegeer@gmail.com>
2024-09-06 13:42:14 -0700

    exported from Skill Struck

---
+++ ITS Python 1/converting.py
@@ -0,0 +1,20 @@
+# string = "23"
+# integer = 23
+# flt = 23.0
+# string2 = int(flt)
+# integer2 = int(string)
+# flt2 = float(integer)
+# print(string2)
+# print(integer2)
+# print(flt2)
+
+# age = int(input())
+# print(age + 18)
+
+# num = float(input())
+# num = int(num)
+# print(num)
+
+# name = input()
+# num = int(input())
+# print(name * num)
