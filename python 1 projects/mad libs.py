9bab5673c85298a393667637f773f343c7296b41
levidegeer <levidegeer@gmail.com>
2024-09-06 13:42:16 -0700

    exported from Skill Struck

---
+++ python 1 projects/mad libs.py
@@ -0,0 +1,15 @@
+noun1 = input("Choose a noun: ")
+pnoun1 = input("Choose a plural noun: ")
+noun2 = input("Choose a noun: ")
+place = input("Choose a place: ")
+adjective = input("Choose an adjective: ")
+
+print("Did you know I have a pet" + noun1 + "?" + "\n" +
+
+"He likes to run around and play with all of the" + pnoun1 + "\n" +
+
+"One morning, I woke up and he was wearing a" + noun2 + "for a hat!" + "\n" +
+
+"I especially like to take him to the" + place + "\n" +
+
+"because he shows off his" + adjective + "side")
