2c89e9046525015d1a18770e88607bbabf991c60
levidegeer <levidegeer@gmail.com>
2024-09-06 13:42:14 -0700

    exported from Skill Struck

---
+++ ITS Python 1/numbers.py
@@ -0,0 +1,37 @@
+# number = 15
+# flt = 3.0
+
+# feet_in_meters = 3.28084
+# print(feet_in_meters)
+
+# hamburger = 8
+# candy = 1
+# movie_ticket = 7
+# video_game = 20
+# hamburger1 = 7.85
+# candy1 = 0.95
+# movie_ticket1 = 6.99
+# video_game1 = 19.95
+
+# rank_outdoors = 8
+# rank_indoors = 8
+# time_outdoors = 0.10
+# time_indoors = 0.90
+# print(rank_outdoors)
+# print(rank_indoors)
+# print(time_outdoors)
+# print(time_indoors)
+
+# var1 = 1
+# var2 = 2
+# var3 = 3
+# var4 = 4
+# var5 = 5
+# flt1 = 1.0
+# flt2 = 2.0
+# flt3 = 3.0
+# flt4 = 4.0
+# flt5 = 5.0
+
+# average_pets = 6.5
+# print(average_pets)
