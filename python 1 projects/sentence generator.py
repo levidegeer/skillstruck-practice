d0fff8157ad25669b8552dc80c6fb04e5bf4a1c7
levidegeer <levidegeer@gmail.com>
2024-09-06 13:42:21 -0700

    exported from Skill Struck

---
+++ python 1 projects/sentence generator.py
@@ -0,0 +1,3 @@
+nouns = ["a" + "b" + "c" + "d" + "e" + "f" + "g" + "h" + "i" + "j"]
+for x in nouns:
+    print("While I was taking my " + x + " for a walk, another giant " + x + " came out of nowhere and chased me all the way home!")
