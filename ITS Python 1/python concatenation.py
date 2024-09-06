cec11ae399fb5a7da2d68a304c70e2163b409754
levidegeer <levidegeer@gmail.com>
2024-09-06 13:42:15 -0700

    exported from Skill Struck

---
+++ ITS Python 1/python concatenation.py
@@ -0,0 +1,31 @@
+# a = "a"
+# b = "b"
+# c = "c"
+# abc = a + b + c
+# print(abc)
+
+# name = input()
+# age = input()
+# date = input()
+# print("Hi " + name + "! You are " + age + " years old and you were born on " + date + ".")
+
+# number = input()
+# word = input()
+# print(number + word)
+
+# animal = input()
+# place = input()
+# plural_noun = input()
+# adjective = input()
+# print("Today I rode a " + animal + " to the " + place + " to buy some " + plural_noun + ". It was " + adjective + ".")
+
+# snickers = input()
+# nerds = input()
+# butterfingers = input()
+# total = int(snickers) + int(nerds) + int(butterfingers)
+# print("This year, you got " + snickers + " snickers, " + nerds + " nerds, and " + butterfingers + " butterfingers. The total number of these candies is " + str(total) + " candies.")
+
+# recipient = input("Who is the message for?")
+# place = input("Where do you want to meet?")
+# minutes = input("In how many minutes would you like to meet?")
+# print("I have an important message for " + recipient + ". Meet me at the " + # place + " in " + minutes + " minutes. From, Detective Buggy.")
