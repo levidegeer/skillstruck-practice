9dd0a31a236f42da1c69fa1e01f0bf3a5acb8171
levidegeer <levidegeer@gmail.com>
2024-09-06 13:42:19 -0700

    exported from Skill Struck

---
+++ python 1 projects/team roster.py
@@ -0,0 +1,13 @@
+print("Joe" + str(10))
+print("Madeline" + str(32))
+print("Jeremy" + str(29))
+print("Ahmed" + str(19))
+print("Li" + str(78))
+print("Sarah" + str(90))
+print("Xander" + str(55))
+print("Jane" + str(72))
+print("Savannah" + str(11))
+print("Juan" + str(64))
+
+string1 = "Welcome to the team! Your team number is {}"
+print(string1.format(number))
