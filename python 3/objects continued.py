b4d4ca41bc01b31eeee53b0c3dc5c7fd7f810b89
levidegeer <levidegeer@gmail.com>
2024-09-13 12:32:21 -0700

    exported from Skill Struck

---
+++ python 3/objects continued.py
@@ -0,0 +1,42 @@
+# class Vacation:
+#     def __init__(self, place, distance, weather):
+#         self.place = place
+#         self.distance = distance
+#         self.weather = weather
+# 
+#     def tuesday(self):
+#         print("We will be hiking on Tuesday.")
+# summer = Vacation("Hawaii", 2000, "Sunny")
+# summer.rating = 10
+# summer.weather = "rainy"
+# print(summer)
+# print(summer.rating)
+# print(summer.weather)
+
+# class Friday:
+#     def __init__(self, activity, friend):
+#         self.activity = activity
+#         self.friend = friend
+# 
+#         def pictures(self):
+#             print("We took so many pictures!")
+# thisWeekend = Friday("Movie", "Charlotte")
+# thisWeekend.money = 20
+# thisWeekend.friend = "Shane"
+# print(thisWeekend)
+# print(thisWeekend.money)
+# print(thisWeekend.friend)
+
+# class Shopping:
+#     def __init__(self, item, quality):
+#         self.item = item
+#         self.quality = quality
+#         self.total = []
+# 
+#     def spending(self, cost):
+#         self.total.append(cost)
+# sportStore = Shopping("Kayak", "High Quality")
+# sportStore.spending(6)
+# sportStore.spending(234)
+# sportStore.spending(76)
+# print(sportStore.total)
