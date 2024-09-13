268c002bb6b0e8a277c6d70944266793cab2751f
levidegeer <levidegeer@gmail.com>
2024-09-13 12:32:24 -0700

    exported from Skill Struck

---
+++ python 3/bubble sort.py
@@ -0,0 +1,11 @@
+# arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+# arr.append(int(input()))
+# def BubbleSort(sort_list):
+#     n = len(sort_list)
+#     for i in range(n-1):
+#         for j in range(0, n-i-1):
+#             if sort_list[j] >= sort_list[j+1]:
+#                 sort_list[j], sort_list[j+1] = sort_list[j+1], sort_list[j]
+# 
+# BubbleSort(arr)
+# print(arr)
