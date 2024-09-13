18fc44712507484170af5bcf3b39c205bcdad9e4
levidegeer <levidegeer@gmail.com>
2024-09-13 12:32:41 -0700

    exported from Skill Struck

---
+++ ITS Python 1/string methods.py
@@ -0,0 +1,20 @@
+# s1 = "a"
+# s2 = "B"
+# s3 = "c"
+# print(s1.upper())
+# print(s2.lower())
+# print(s3.replace("c", "d"))
+
+# string = input()
+# x = string.replace("1", "one")
+# print(x)
+
+# x = input()
+# print(x.upper())
+
+# string = input()
+# string = string.replace("I", "We")
+# print(string)
+
+# string = input()
+# print(string.replace("@", ""))
