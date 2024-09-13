38e95a3969988a91c08948a35831761da1b3a185
levidegeer <levidegeer@gmail.com>
2024-09-13 12:32:40 -0700

    exported from Skill Struck

---
+++ ITS Python 1/strings.py
@@ -0,0 +1,38 @@
+# var = """qwertyuip.
+# oiuyt"""
+
+# short_quote = "shortquote"
+# long_quote = """long 
+# quote"""
+# print(short_quote)
+# print(long_quote)
+
+# journal = """test
+# test"""
+# print(journal)
+
+# ideas = """a b
+# c d"""
+# print(ideas)
+
+# name1 = "a"
+# name2 = "b"
+# name3 = "c"
+# name4 = "d"
+# print(name1)
+# print(name2)
+# print(name3)
+# print(name4)
+
+# fruit1 = "fa"
+# fruit2 = "fb"
+# fruit3 = "fc"
+# veggie1 = "va"
+# veggie2 = "vb"
+# veggie3 = "vc"
+# print(fruit1)
+# print(fruit2)
+# print(fruit3)
+# print(veggie1)
+# print(veggie2)
+# print(veggie3)
