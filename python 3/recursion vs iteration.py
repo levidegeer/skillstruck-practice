ef82ac103ea4b1acfc6ec543bd65f1d71a3ab7ea
levidegeer <levidegeer@gmail.com>
2024-09-13 12:32:23 -0700

    exported from Skill Struck

---
+++ python 3/recursion vs iteration.py
@@ -0,0 +1,26 @@
+# def factorial(n):
+#     if n <= 1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+# print(factorial(int(input())))
+
+# list_of_nums = [int(n) for n in input().split()]
+# def add_list(my_list):
+# 	if len(my_list) == 1:
+# 		return my_list[0]
+# 	else:
+#             x = my_list.pop(0)
+#             return x + add_list(my_list)
+# print(add_list(list_of_nums))
+
+# x = int(input())
+# def fibonacci(num):
+#     if num <= 1:
+#         return num
+#     else:
+#         return(fibonacci(num-1) + fibonacci(num-2))
+# list = []
+# for i in range(x):
+#     list.append(fibonacci(i))
+# print(list)
