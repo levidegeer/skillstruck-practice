1e0013943b187998ce90bea6ab432e6f58f53ed0
levidegeer <levidegeer@gmail.com>
2024-09-13 12:32:42 -0700

    exported from Skill Struck

---
+++ ITS Python 1/string methods continued.py
@@ -0,0 +1,43 @@
+# string1 = "asdfghjkl"
+# print(string1[4:6])
+# print(string1.split())
+# print(len(string1))
+# print(string1.find("s"))
+
+# string = input()
+# x = string.split()
+# print(len(x))
+
+# string = input()
+# a = string.find("e")
+# b = string.rfind("e")
+# print(str(a) + "-" + str(b))
+
+# string = input()
+# print(string[2] + string[-3] + string[3])
+
+# string = input()
+# print(string.rfind("g"))
+
+# string = input()
+# l = len(string)
+# c = l // 2
+# a = string[:c]
+# b = string[c:]
+# print(b + a)
+
+# string = input()
+# j1 = string.find("j")
+# j2 = string.rfind("j")
+# a = string[:j1]
+# b = string[j2+1:]
+# print(a + b)
+
+# string = input()
+# reverse = string[len(string)::-1]
+# j1 = reverse.find("j")
+# j2 = reverse.rfind("j")
+# a = reverse[j1:j2+1]
+# print(a)
+
+
