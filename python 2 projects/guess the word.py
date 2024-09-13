dd47cde67b14f4106e938cd9f0a47aa204b05458
levidegeer <levidegeer@gmail.com>
2024-09-13 12:32:34 -0700

    exported from Skill Struck

--- python 2 projects/guess the word.py
+++ python 2 projects/guess the word.py
@@ -1,17 +1,13 @@
-word_lst = ["For", "this", "project", "you", "will", "create", "list", "of", "words", "which", "will"]
+my_lst = ["For", "this", "project", "you", "will", "create", "list", "of", "words", "which", "will"]
 number = int(input())
-word = word_lst[number]
-word_l = []
-word_l.extend(word)
+word = my_lst[number]
+lst = list(word)
 turns = 12
 while turns > 0:
-    myletter = input()
-    print("")
-    for x in word_l:
-        if myletter == x:
-            print(myletter)
-        else:
-            print("-")
+    myletter = input("Input a letter")
+    if myletter in lst:
+        print(myletter)
+    else:
+        print("-")
     turns -= 1
 
-print("You ran out of turns")
