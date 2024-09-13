49eed3e1a8a9e6eb2bb051385d31bf82276ab04a
levidegeer <levidegeer@gmail.com>
2024-09-13 12:32:39 -0700

    exported from Skill Struck

---
+++ ITS Python 1/modulus.py
@@ -0,0 +1,20 @@
+# num = int(input())
+# print(num % 7)
+
+# people = int(input())
+# oranges = int(input())
+# r = oranges % people
+# print(r)
+
+# num = int(input())
+# print(num%100)
+
+# num = int(input())
+# num = str(num)
+# print(num[0] + " " + num[1])
+
+# num = input()
+# print(num[::-1])
+
+#num = input()
+#print(num[-2])
