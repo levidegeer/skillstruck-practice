02b44048811ce95eca5d70c906a8cd491b21e870
levidegeer <levidegeer@gmail.com>
2024-09-20 12:30:30 -0700

    exported from Skill Struck

---
+++ ITS Python 1/concatenating numbers.py
@@ -0,0 +1,29 @@
+# string = "test{}test"
+# integer = 5
+# print(string.format(integer))
+
+# age = 17
+# birthday = "{}"
+# print(birthday.format(age))
+
+# t = 8
+# apt = int(input())
+# var = "If Bob harvested one tree, he would have " + str(apt/3) + " apples per wheelbarrow. If he harvested the whole orchard, he would have " + str((apt/3)*8) + " apples per wheelbarrow."
+# print(var)
+
+# m = int(input())
+# h = m // 60
+# m = m - h*60
+# print("It has been " + str(h) + " hour(s) and " + str(m) + " minute(s) since midnight." )
+
+# x = int(input())
+# a = (x+1) // (2)
+# string = "Minimum number of tables: {}"
+# print(string.format(a))
+
+jb = int(input())
+cb = int(input())
+
+string = "If Jasmine had 9 bags, she would have 108 marbles total. If Chloe had 10 bags, she would have 100 marbles total."
+
+
