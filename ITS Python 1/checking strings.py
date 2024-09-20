1c07343973d2e96ae375deb771d5fc882c963b95
levidegeer <levidegeer@gmail.com>
2024-09-20 12:30:30 -0700

    exported from Skill Struck

---
+++ ITS Python 1/checking strings.py
@@ -0,0 +1,19 @@
+# string = "qwertyuiop"
+# check = "qwerty" in string
+# print(check)
+
+# string = input()
+# check = "armadillo" in string
+# print(check)
+
+# string = input()
+# check = "a" in string
+# print(check)
+
+# string = input()
+# check = "1" in string
+# print(check)
+
+# passage = "Cassowaries, genus Casuarius, are ratites (flightless birds without a keel on their sternum bone) that are native to the tropical forests of New Guinea (Papua New Guinea and Indonesia), East Nusa Tenggara, the Maluku Islands, and northeastern Australia. There are three extant species. The most common of these, the southern cassowary, is the third-tallest and second-heaviest living bird, smaller only than the ostrich and emu. Cassowaries feed mainly on fruit, although all species are truly omnivorous and will take a range of other plant food, including shoots and grass seeds, in addition to fungi, invertebrates, and small vertebrates. Cassowaries are very wary of humans, but if provoked they are capable of inflicting serious, even fatal, injuries to both dogs and people. The cassowary has often been labeled the world's most dangerous bird. Typically, all cassowaries are shy birds that are found in the deep forest. They are adept at disappearing long before a human knows they were there."
+# check = "dangerous" in passage
+# print(check)
