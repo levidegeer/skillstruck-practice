2326c3e1d0ff39e9f72fc06e87038d5095668d2b
levidegeer <levidegeer@gmail.com>
2024-09-27 13:41:04 -0700

    exported from Skill Struck

---
+++ ITS Python 1/python if statements.py
@@ -0,0 +1,38 @@
+# x = int(input())
+# if x < 12:
+#     print("Good Morning!")
+# else:
+#     print("Good Day!")
+
+# a = int(input())
+# b = int(input())
+# if a < b:
+#     print(a)
+# else:
+#     print(b)
+
+# x = int(input())
+# if x >= 0:
+#     print(1)
+# else:
+#     print(-1)
+
+# x = input()
+# if len(x) == 3:
+#     print("Yes")
+# else:
+#     print("No")
+
+# x = int(input())
+# if x % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+a = 15
+if a % 3 == 0:
+    print("Is divisible")
+else:
+    print("Not divisible")
+
+
