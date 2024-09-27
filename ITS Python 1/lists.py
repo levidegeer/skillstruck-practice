46a8bab1ef869b7d25b1be8956c430daad1f3e9f
levidegeer <levidegeer@gmail.com>
2024-09-27 13:41:03 -0700

    exported from Skill Struck

--- ITS Python 1/lists.py
+++ ITS Python 1/lists.py
@@ -3,7 +3,17 @@
 # print(candy)
 # print(pieces)
 
+# friends = ["a" , "b" , "c"]
+# print(friends)
 
+# reasons = ["a" , "b" , "c"]
+# print(reasons)
 
+# ages = [1, 2, 3, 4]
+# print(ages)
 
+# places = ["a", "b", "c", "d", "e"]
+# print(places)
 
+# memories = [0, 1, "a", "b", 2]
+# print(memories)
